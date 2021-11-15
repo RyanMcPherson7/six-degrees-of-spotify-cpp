e43550b2de21fa969d145994b5366fd0ab431a02
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-11-15 16:23:26 -0500

    updated seeding values

--- src/app.js
+++ src/app.js
@@ -7,16 +7,14 @@ let artistIdSet = new Set();
 processQ.enqueue(['Justin Bieber', '1uNFoZAHBGtllmzznpCI3s']);
 processQ.enqueue(['Kanye West', '5K4W6rqBFWDnAN6FQUkS6x']);
 processQ.enqueue(['The Beatles', '3WrFJ7ztbogyGnTHbHJFl2']);
-processQ.enqueue(['Avicci', '1vCWHaC5f2uS3yhpwWbIA6']);
-processQ.enqueue(['Empire of the Sun', '67hb7towEyKvt5Z8Bx306c']);
 processQ.enqueue(['Michael Jackson', '3fMbdgg4jU18AjLCKBhRSm']);
 processQ.enqueue(['Tame Impala', '5INjqkS1o8h1imAzPqGZBb']);
 processQ.enqueue(['Bad Bunny', '4q3ewBCX7sLwd24euuV69X']);
 
 // populating data file
 const NUM_ARTISTS = 13000;
-const ARTIST_PER_REQUEST = 400;
-const MINUTE_DELAY = 5;
+const ARTIST_PER_REQUEST = 500;
+const MINUTE_DELAY = 10;
 
 for (let i = 0; i < NUM_ARTISTS / ARTIST_PER_REQUEST; i++) {
   setTimeout(() => {
